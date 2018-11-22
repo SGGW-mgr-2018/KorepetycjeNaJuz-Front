1489ebf682fdf94fd20ef253da1e94abe030b094
Adam <rumpelteam@gmail.com>
2018-11-23 00:10:19 +0100

    Fix displaying login modal

--- korepetycje-front/src/components/Login/LoginModal.vue
+++ korepetycje-front/src/components/Login/LoginModal.vue
@@ -3,13 +3,15 @@
     <div class="login-modal__wrapper">
       <input type="text" class="login-modal__input" placeholder="LOGIN">
       <input type="text" class="login-modal__input" placeholder="HASŁO">
-      <router-link to="/" class="login-modal__link">Zapomniałeś hasła?</router-link>
+      <router-link to="/" class="login-modal__link">
+        Zapomniałeś hasła?
+      </router-link>
       <button-component class="login-modal__button" pink>
         Zaloguj się
       </button-component>
-      <div class="login-modal__socialButtons">
-        <img src="/svg/facebook_icon.svg" alt="Facebook" class="img">
-        <img src="/svg/google_plus_icon.svg" alt="Google+" class="img">
+      <div class="social-buttons">
+        <img src="/svg/facebook_icon.svg" alt="Facebook" class="social-buttons__button">
+        <img src="/svg/google_plus_icon.svg" alt="Google+" class="social-buttons__button">
       </div>
     </div>
   </div>
@@ -17,6 +19,7 @@
 
 <script>
 import ButtonComponent from '@/components/Button'
+
 export default {
   name: 'LoginModal',
   components: {
@@ -27,44 +30,54 @@ export default {
 
 <style lang="scss" scoped>
   .login-modal {
-     width: 200px;
-     background-color: #fff;
-     border-radius: 5px;
-         margin-left: auto;
+    width: 200px;
+    padding-top: 10px;
 
     &__wrapper {
       display: flex;
       flex-direction: column;
       justify-content: space-around;
       padding: 15px 8px;
+      background-color: #fff;
+      border-radius: 5px;
     }
-     &__input {
+
+    &__input {
       max-width: 400px;
       width: 100%;
       padding: 7px;
       border: 1px solid #bfbfbf;
-      font-size: 10px;
-        margin-bottom: 8px;
+      font-size: 14px;
+      margin-bottom: 8px;
     }
-     &__link {
-         text-decoration: none;
-         color: #6c6c6c;
-         font-size: 9px;
-         font-weight: normal;
-        text-align: center;
+
+    &__link {
+      text-decoration: none;
+      color: #6c6c6c;
+      font-size: 12px;
+      font-weight: normal;
+      text-align: center;
+    }
+
+    &__button {
+      font-size: 14px;
+      margin: 9px 16px;
+    }
+
+    .social-buttons {
+      display: flex;
+      justify-content: space-around;
+      margin: 0 50px;
+
+      &__button {
+        width: 30px;
+        height: 30px;
+        cursor: pointer;
+
+        &:hover {
+          opacity: .8;
+        }
       }
-     &__button {
-        font-size: 12px;
-        margin: 9px 16px;
-     }
-     &__socialButtons {
-        display: flex;
-        justify-content: space-around;
-        margin: 0 50px;
-     }
+    }
   }
-   .img {
-      width: 30px;
-      height: 30px;
-   }
 </style>
--- korepetycje-front/src/components/NavBar.vue
+++ korepetycje-front/src/components/NavBar.vue
@@ -1,14 +1,18 @@
 <template>
-  <div class="nav">
+  <div class="nav" @mouseleave="showedPopup = false">
     <grid-container class="nav__wrapper">
       <img src="" alt="Logo" class="nav__logo">
       <div class="nav-links">
         <router-link to="/" class="nav-links__link">Główna</router-link>
         <router-link to="/rejestracja" class="nav-links__link">Rejestracja</router-link>
-        <router-link to="/" class="nav-links__link log" @mouseover.native="showedPopup = true" @mouseleave.native="showedPopup = false">Logowanie</router-link>
+        <router-link
+          to="/logowanie"
+          class="nav-links__link log"
+          @mouseover.native="showedPopup = true"
+        >
+          Logowanie
+        </router-link>
       </div>
-    </grid-container>
-    <grid-container>
       <login-modal v-show="showedPopup" />
     </grid-container>
   </div>
@@ -36,6 +40,7 @@ export default {
     width: 100%;
 
     &__wrapper {
+      position: relative;
       display: flex;
       justify-content: space-between;
     }
@@ -44,8 +49,9 @@ export default {
   .nav-links {
     display: flex;
     justify-content: flex-end;
+
     &__link {
-      margin: 10px 0px;
+      margin: 10px 0;
       padding: 10px 40px;
       font-size: 17px;
       text-decoration: none;
@@ -59,6 +65,21 @@ export default {
       &.router-link-exact-active {
         font-weight: 500;
       }
+    }
+  }
+
+  .login-modal {
+    position: absolute;
+    top: 60px;
+    right: 0;
+    margin-right: 60px;
+
+    @include tablet {
+      margin-right: 40px;
+    }
+
+    @include mobile {
+      display: none;
     }
   }
 </style>
