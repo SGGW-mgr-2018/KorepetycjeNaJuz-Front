619a63f7bc68db33fb717d73fcec2daaa6850e0a
Maja Marchewka <marchewka.maja@gmail.com>
2018-11-29 19:33:07 +0100

    Completed user about page

--- korepetycje-front/src/views/UserAbout.vue
+++ korepetycje-front/src/views/UserAbout.vue
@@ -1,27 +1,37 @@
 <template>
   <div class="page--user">
     <grid-container class="page-container">
-      <img src="/svg/pen_purple.svg" class="pencil left" alt="Pencil">
-      <img src="/svg/pen_purple.svg" class="pencil right" alt="Pencil">
-      <h1>Informacje o użytkowniku</h1>
-      <img :src="user.avatar" class="user-avatar" alt="Avatar">
-      <div class="user-details">
-        <h2>{{ user.firstName }} {{ user.lastName }}</h2>
-        <div class="user-about">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend tempus iaculis. Praesent interdum, magna ac sagittis convallis, risus ex condimentum ipsum, sed luctus nulla ipsum ac augue. Nullam egestas nulla eu eros sagittis feugiat. Nulla eget risus eu neque ullamcorper sollicitudin. Aenean sed turpis at libero pulvinar pulvinar eget eu mi. Ut vitae eleifend ligula, at convallis massa. Aliquam erat volutpat.
+      <h1 class="main-header">Informacje o użytkowniku</h1>
+      <div class="content-container">
+        <img :src="user.avatar" class="user-avatar" alt="Avatar">
+        <div class="user-details">
+          <h2 class="user-name">{{ user.firstName }} {{ user.lastName }}</h2>
+          <div class="user-about">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend tempus iaculis. Praesent interdum, magna ac sagittis convallis, risus ex condimentum ipsum, sed luctus nulla ipsum ac augue. Nullam egestas nulla eu eros sagittis feugiat. Nulla eget risus eu neque ullamcorper sollicitudin. Aenean sed turpis at libero pulvinar pulvinar eget eu mi. Ut vitae eleifend ligula, at convallis massa. Aliquam erat volutpat.
+          </div>
+          <div class="star-rating">
+            <span>Średnia ocena:</span>
+            <star-rating
+              v-model="user.rating"
+              :inline="true"
+              :show-rating="false"
+              :border-width="2"
+              :border-color="'#000'"
+              :star-size="30"
+              read-only
+            />
+          </div>
+          <div>
+            <div class="active">Aktualne korepetycje:</div>
+            <div v-for="lesson in user.lessons" :key="lesson.id" class="lesson">
+              <h2 class="lesson-title">{{ lesson.lessonSubject }}, {{ lesson.date }}, {{ lesson.time }}</h2>
+              <div class="description">{{ lesson.description }}</div>
+              <button-component pink class="button-enroll" @click="enroll">
+                ZAPISZ SIĘ
+              </button-component>
+            </div>
+          </div>
         </div>
-        <div class="star-rating">
-          Średnia ocena:
-          <star-rating v-model="user.rating" :inline="true" :show-rating="false" read-only />
-        </div>
-        <div>
-          <div class="active">Aktualne korepetycje:</div>
-          <h2>Matematyka, 23.09.2018, 16:00-17:30</h2>
-          <div class="description">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend tempus iaculis. Praesent interdum, magna ac sagittis convallis, risus ex condimentum ipsum, sed luctus nulla ipsum ac augue. Nullam egestas nulla eu eros sagittis feugiat. Nulla eget risus eu neque ullamcorper sollicitudin. Aenean sed turpis at libero pulvinar pulvinar eget eu mi. Ut vitae eleifend ligula, at convallis massa. Aliquam erat volutpat.</div>
-        </div>
-        <button-component pink class="button-enroll" @click="enroll">
-          ZAPISZ SIĘ
-        </button-component>
       </div>
     </grid-container>
   </div>
@@ -43,7 +53,16 @@ export default {
         firstName: 'Jan',
         lastName: 'Kowalski',
         avatar: 'https://www.shareicon.net/download/2017/02/05/878221_user_512x512.png',
-        rating: 3
+        rating: 3,
+        lessons: [
+          {
+            id: 5,
+            lessonSubject: 'Matematyka',
+            date: '23.09.2018',
+            time: '16:00-17:30',
+            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eleifend tempus iaculis. Praesent interdum, magna ac sagittis convallis, risus ex condimentum ipsum, sed luctus nulla ipsum ac augue. Nullam egestas nulla eu eros sagittis feugiat. Nulla eget risus eu neque ullamcorper sollicitudin. Aenean sed turpis at libero pulvinar pulvinar eget eu mi. Ut vitae eleifend ligula, at convallis massa. Aliquam erat volutpat.'
+          }
+        ]
       }
     }
   },
@@ -61,14 +80,77 @@ export default {
 <style lang="scss" scoped>
   .page--user {
     padding: 100px 0;
-    //text-align: center;
   }
 
   .page-container {
     position: relative;
-    z-index: 5;
     max-width: 915px;
     margin: 0 auto;
+
+    &:before {
+      content: '';
+      background: url("/svg/pen_purple.svg") no-repeat;
+      width: 93px;
+      height: 120px;
+      background-size: contain;
+      position: absolute;
+      transform: scaleX(-1);
+      bottom: 0;
+      left: 60px;
+
+      @include mobile() {
+        display: none;
+      }
+    }
+
+    &:after {
+      content: '';
+      background: url("/svg/pen_purple.svg") no-repeat;
+      width: 78px;
+      height: 90px;
+      background-size: contain;
+      position: absolute;
+      top: 148px;
+      right: 10px;
+
+      @include mobile() {
+        display: none;
+      }
+    }
+  }
+
+  .main-header {
+    padding-left: 93px;
+
+    @include mobile() {
+      padding-left: 0;
+      text-align: center;
+    }
+  }
+
+  .content-container {
+    display: flex;
+    position: relative;
+    z-index: 1;
+
+    @include mobile() {
+      flex-direction: column;
+
+      .user-details {
+        padding-left: 0;
+        margin-top: 15px;
+      }
+    }
+  }
+
+  .lesson {
+    clear: both;
+    margin-bottom: 45px;
+  }
+
+  .user-name {
+    padding-top: 0;
+    margin-top: 0;
   }
 
   h1, h2, h3 {
@@ -105,36 +187,14 @@ export default {
     }
   }
 
-  .pencil {
-    position: absolute;
-    top: 180px;
-    z-index: 1;
-    display: inline-block;
-    color: #ddd8ff;
-
-    &.left {
-      top: 420px;
-      left: -50px;
-      width: 78px;
-      height: 90px;
-      transform: scaleX(-1);
-    }
-
-    &.right {
-      right: -20px;
-      width: 56px;
-      height: 48px;
-    }
-  }
-
   .user-avatar {
     width: 93px;
     height: 93px;
     border-radius: 100%;
-    position: absolute;
-    display: inline;
-    top: 100px;
-    left: -40px;
+
+    @include mobile() {
+      margin: auto;
+    }
   }
 
   .user-about {
@@ -145,16 +205,29 @@ export default {
   .star-rating {
     font-weight: 500;
     color: var(--greyish-brown);
+    display: flex;
+    align-items: center;
+    margin-top: 15px;
+
+    > span {
+      display: inline-block;
+      margin-right: 15px;
+    }
+  }
+
+  .lesson-title {
+    font-weight: normal;
+    margin-bottom: 0;
   }
 
   .description {
     text-align: justify;
     font-size: 14px;
-    padding-bottom: 43px;
+    padding-bottom: 20px;
   }
 
   .user-details {
-    padding-left: 20px;
+    padding-left: 30px;
     display: inline-block;
   }
 
@@ -163,18 +236,6 @@ export default {
     font-weight: 500;
     color: var(--greyish-brown);
     padding-top: 43px;
-  }
-
-  @include mobile {
-    .pencil {
-      display: none;
-    }
-
-    .user-avatar {
-      display: block;
-      margin-left: auto;
-      margin-right: auto;
-    }
   }
 
   .button {
