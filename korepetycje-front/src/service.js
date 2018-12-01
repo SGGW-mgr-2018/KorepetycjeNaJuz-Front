fa3763048592bfa4ee24477808995aac1ad2d381
Adam <rumpelteam@gmail.com>
2018-12-01 13:25:08 +0100

    Add login to backend from Login page

--- korepetycje-front/src/service.js
+++ korepetycje-front/src/service.js
@@ -2,8 +2,22 @@ import axios from 'axios'
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL
 
+const handleErrors = err => {
+  if (!err.response || err.response.status === 500) {
+    return
+  }
+
+  if (err.response.status === 401) {
+    return
+  }
+
+  return err.response
+}
+
+const responseData = res => res
+
 const auth = {
-  login: payload => axios.post('/api/Authorization/Login', payload)
+  login: payload => axios.post('/api/Authorization/Login', payload).then(responseData).catch(handleErrors)
 }
 
 export default {
