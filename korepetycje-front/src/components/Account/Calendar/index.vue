b2266c5c62b4b95bec715e3562b184f4bf793295
Adam <rumpelteam@gmail.com>
2019-01-03 23:30:23 +0100

    Add Vue Calendar

--- korepetycje-front/src/components/Account/Calendar/index.vue
+++ korepetycje-front/src/components/Account/Calendar/index.vue
@@ -1,10 +1,43 @@
 <template>
-  <div>Kalendarz</div>
+  <div>
+    Kalendarz
+    <full-calendar :config="config" :events="events" />
+  </div>
 </template>
 
 <script>
+import { FullCalendar } from 'vue-full-calendar'
+import moment from 'moment'
+import 'fullcalendar/dist/fullcalendar.css'
+
 export default {
-  name: 'Calendar'
+  name: 'Calendar',
+  components: {
+    FullCalendar
+  },
+  data () {
+    return {
+      events: [
+        {
+          title: 'Test',
+          allDay: true,
+          start: moment(),
+          end: moment().add(1, 'd')
+        },
+        {
+          title: 'Kolejny test',
+          start: moment().add(2, 'd'),
+          end: moment().add(2, 'd').add(2, 'h')
+        }
+      ],
+      config: {
+        defaultView: 'month',
+        eventRender (event, element) {
+          console.log(event)
+        }
+      }
+    }
+  }
 }
 </script>
 
