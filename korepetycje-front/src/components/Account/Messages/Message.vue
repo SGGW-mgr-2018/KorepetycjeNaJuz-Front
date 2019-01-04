2080e9b3ff0bb13774f6233a4b60e99840cc4cd7
Adam <rumpelteam@gmail.com>
2019-01-04 23:24:56 +0100

    Working on messages list

---
+++ korepetycje-front/src/components/Account/Messages/Message.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="message">
+    <header class="message__header">
+      <p class="message__title">{{ message.title }}</p>
+      <p class="message__date">{{ message.date }}</p>
+    </header>
+    <p class="message__content">{{ message.content }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  props: {
+    message: {
+      type: Object,
+      default: () => ({
+        title: 'Tytuł wiadomości',
+        date: '01/01/2019',
+        content: 'To jest jakaś wiadomość. Lepiej na nią odpowiedz. Tak zrób.'
+      })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .message {
+    &__header {
+
+    }
+
+    &__title {
+
+    }
+
+    &__content {
+
+    }
+  }
+</style>
--- korepetycje-front/src/components/Account/Messages/index.vue
+++ korepetycje-front/src/components/Account/Messages/index.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>Wiadomości</div>
+  <div>
+    <div class="messages">
+      <message-shortcut
+        v-for="number in 3"
+        :key="number"
+        @click="goToMessage(number)"
+      />
+    </div>
+    <div class="message-wrapper">
+      <button class="arrow-back" @click="showMessagesList">&larr;</button>
+      <message />
+    </div>
+  </div>
 </template>
 
 <script>
+import MessageShortcut from './MessageShortcut'
+import Message from './Message'
 export default {
-  name: 'Messages'
+  name: 'Messages',
+  components: {
+    Message,
+    MessageShortcut
+  },
+  methods: {
+    goToMessage (id) {
+      console.log('message ID', id)
+    },
+    showMessagesList () {
+      console.log('Show messages list')
+    }
+  }
 }
 </script>
 
