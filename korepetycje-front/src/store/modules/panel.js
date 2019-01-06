0e5b779507993586c8c8c0557721654c69906bd1
Adam <rumpelteam@gmail.com>
2019-01-06 22:46:06 +0100

    Move functions to JS utils

--- korepetycje-front/src/store/modules/panel.js
+++ korepetycje-front/src/store/modules/panel.js
@@ -1,7 +1,11 @@
 import service from '@/service'
-import moment from 'moment'
-import _get from 'lodash.get'
 import _uniqBy from 'lodash.uniqby'
+import {
+  createStudentLesson,
+  createTeacherLesson,
+  createTeacherContact,
+  createStudentContact
+} from '@/assets/js/utils'
 
 const state = {
   fetchLoading: false,
@@ -17,48 +21,6 @@ const getters = {
     return _uniqBy(state.addressBook, 'id')
   }
 }
-
-const createTeacherContact = payload => ({
-  id: payload.coachId,
-  firstName: payload.coachFirstName,
-  lastName: payload.coachLastName,
-  userType: 2
-})
-
-const createStudentContact = payload => ({
-  id: payload.id,
-  firstName: payload.firstName,
-  lastName: payload.lastName,
-  userType: 1
-})
-
-const createBaseLesson = payload => ({
-  lessonSubject: payload.lessonSubject,
-  description: payload.description,
-  lessonLevels: payload.lessonLevels,
-  time: payload.time,
-  dateStart: payload.dateStart,
-  dateEnd: payload.dateEnd,
-  ratePerHour: payload.ratePerHour
-})
-
-const createStudentLesson = payload => ({
-  ...createBaseLesson(payload),
-  userType: 1,
-  coachId: payload.coachId,
-  coachFirstName: payload.coachFirstName,
-  coachLastName: payload.coachLastName
-})
-
-const createTeacherLesson = (payload, participantInfo = null) => ({
-  ...createBaseLesson(payload),
-  userType: 2,
-  studentId: _get(participantInfo, 'student.id', null),
-  studentFirstName: _get(participantInfo, 'student.firstName', null),
-  studentLastName: _get(participantInfo, 'student.lastName', null),
-  dateStart: _get(participantInfo, 'date', null) || payload.dateStart,
-  dateEnd: participantInfo ? moment(this.dateStart).add(participantInfo.numberOfHours, 'h') : payload.dateEnd
-})
 
 const mutations = {
   SET_LESSONS (state, payload) {
